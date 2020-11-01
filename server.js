acae6acd7d183f18f84313f3422857575c0aee13
Eladio Rocha Vizcaino <2983321-EladioRocha@users.noreply.gitlab.com>
2020-11-01 11:54:40 -0600

    Chat con socket.io y nodejs

--- server.js
+++ server.js
@@ -25,54 +25,5 @@ http.listen(3000, () => console.log('Server on'))
  */
 const rooms = {}
 io.on('connection', socket => {
-  socket.on('join room', ({ id, username, roomName }) => {
-    socket.join(roomName)
-    saveDataRoom(id, username, roomName)
-    io.sockets.in(roomName).emit('user joined', { roomName, users: getUsersFromRoom(roomName) })
-  })
-
-  socket.on('message', message => {
-    const { roomName, username } = searchInfoBySocketId(socket.id)
-    io.sockets.in(roomName).emit('message', { message, username })
-  })
-
-  socket.on('disconnect', () => {
-    const { roomName } = searchInfoBySocketId(socket.id)
-    deleteUserFromRoom(socket.id, roomName)
-    io.sockets.in(roomName).emit('user disconnect', { roomName, users: getUsersFromRoom(roomName) })
-  })
+  
 })
-
-function saveDataRoom(id, username, roomName) {
-  let room = rooms[roomName]
-  if(!room) {
-    room = rooms[roomName] = {}
-  }
-  room[id] = username
-  console.log(rooms)
-}
-
-function getUsersFromRoom(roomName) {
-  if(rooms[roomName]) {
-    return Object.values(rooms[roomName])
-  }
-
-  return []
-}
-
-function searchInfoBySocketId(socketId) {
-  for(const roomName in rooms) {
-    const existInRoom = socketId in rooms[roomName]
-    if(existInRoom) {
-      return { roomName, username: rooms[roomName][socketId] }
-    }
-  }
-
-  return { roomName: null, username: null }
-}
-
-function deleteUserFromRoom(socketId, roomName) {
-  if(roomName) {
-    delete rooms[roomName][socketId]
-  }
-}
